17d50b0bda235398ea3ddfb476b95a71080aa779
pmaharjan <mhrjn.prazol@gmail.com>
2024-12-18 19:35:53 -0600

    new file

---
+++ Prazol/scholarship.py
@@ -0,0 +1,10 @@
+age = 18
+grade = 70
+attendance = 70
+
+#conditioanl statement with logical comparsion operators
+if age>=18 and (grade>=80 or attendance>=85):
+    print("You are eligible for the scholarship!")
+else:
+    print("You are not eligible for the scholarship.")
+    
